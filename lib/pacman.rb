4d74ac87abb8c8fa1e38e4e281a00f427980b664
Annmarie De Silva <annmarie.ds@hotmail.com>
2020-11-09 09:50:54 +1100

    Challenge completed

--- lib/pacman.rb
+++ lib/pacman.rb
@@ -1,68 +1,52 @@
 require_relative 'grid'
-
 class Pacman
-
   @@directions = ['NORTH', 'EAST', 'SOUTH', 'WEST'].freeze
-
-  attr_reader :x_coord, :y_coord, :f, :grid
-
+  attr_reader :x_coord, :y_coord, :direction, :grid
   def initialize
     @grid = Grid.new
     @x_coord = nil
     @y_coord = nil
-    @f = nil
+    @direction = nil
   end
-
   def placed?
-    !x_coord.nil? && !y_coord.nil? && !f.nil?
+    !x_coord.nil? && !y_coord.nil? && !direction.nil?
   end
-
-  def place(x_coord, y_coord, f)
+  def place(x_coord, y_coord, direction)
     @x_coord = x_coord
     @y_coord = y_coord
-    @f = f
+    @direction = direction
   end
-
-  # def move_to(x_coord, y_coord, f)
-  #   place(x = x_coord, y_coord = y_coord, f = f)
-  # end
-
   def left
-    p_index = @@directions.index { |d| d == @f }
-
-    if @f == @@directions.first
+    p_index = @@directions.index { |d| d == @direction }
+    if @direction == @@directions.first
       new_dir = @@directions.last
     else
       p_index -= 1
       new_dir = @@directions[p_index]
     end
-    @f = new_dir
+    @direction = new_dir
   end
-
   def right
-    p_index = @@directions.index { |d| d == @f }
-
-    if @f == @@directions.last
+    p_index = @@directions.index { |d| d == @direction }
+    if @direction == @@directions.last
       new_dir = @@directions.first
     else
       p_index += 1
       new_dir = @@directions[p_index]
     end
-    @f = new_dir
+    @direction = new_dir
   end
-
   def report
-    return "#{@x_coord}, #{@y_coord}, #{@f}"
+    return "#{@x_coord}, #{@y_coord}, #{@direction}"
   end
-
   def move
-    if @f == 'NORTH' && @x_coord <= 3
+    if @direction == 'NORTH' && @y_coord <= 3
       @y_coord += 1
-    elsif @f == 'SOUTH' && @y_coord >= 1
+    elsif @direction == 'SOUTH' && @y_coord >= 1
       @y_coord -= 1
-    elsif @f == 'EAST' && @y_coord <= 3
+    elsif @direction == 'EAST' && @x_coord <= 3
       @x_coord += 1
-    elsif @f =='WEST' && @x_coord >= 1
+    elsif @direction =='WEST' && @x_coord >= 1
       @x_coord -= 1
     else
       puts "Invalid - Pacman must move on the grid"
--- spec/pacman_spec.rb
+++ spec/pacman_spec.rb
@@ -1,108 +1,99 @@
 require 'rspec'
 require './lib/pacman'
-
 describe Pacman do
   before do
     @grid = Grid.new
     @pacman = Pacman.new
   end
-
   describe "#initialize" do
     it 'initialized position should be nil' do
       expect(@pacman.x_coord).to be_nil
       expect(@pacman.y_coord).to be_nil
-      expect(@pacman.f).to be_nil
+      expect(@pacman.direction).to be_nil
     end
   end
-
   describe ".placed?" do
     it 'should return false if Pacman initialized correctly' do
       expect(@pacman.placed?).to be false
     end
   end
-
   describe ".place" do
     context 'when pacman is not yet on the grid' do
     x_coord = 1
     y_coord = 2
-    f = 'NORTH'
+    direction = 'NORTH'
       it 'updates position of pacman' do
         expect(@pacman.report).to eq ", , "
-        @pacman.place(x_coord, y_coord, f)
+        @pacman.place(x_coord, y_coord, direction)
         expect(@pacman.x_coord).to eq 1
         expect(@pacman.y_coord).to eq 2
-        expect(@pacman.f).to eq 'NORTH'
+        expect(@pacman.direction).to eq 'NORTH'
       end
     end
   end
-
   describe ".move_to" do
     context 'while pacman is on grid' do
     x_coord = 1
     y_coord = 3
-    f = 'NORTH'
+    direction = 'NORTH'
       it 'updates position of pacman' do
-        @pacman.place(x_coord, y_coord, f)
+        @pacman.place(x_coord, y_coord, direction)
         expect(@pacman.x_coord).to eq 1
         expect(@pacman.y_coord).to eq 3
-        expect(@pacman.f).to eq 'NORTH'
+        expect(@pacman.direction).to eq 'NORTH'
       end
     end
   end
-
   describe ".left" do
     context 'when user inputs left' do
       x_coord = 0
       y_coord = 0
-      f = 'NORTH'
+      direction = 'NORTH'
       it 'should turn pacman but he should not move' do
         @pacman.left
         expect(@pacman.x_coord).to eq 0
         expect(@pacman.y_coord).to eq 0
-        expect(@pacman.f).to eq 'WEST'
+        expect(@pacman.direction).to eq 'WEST'
       end
     end
   end
-
   describe ".right" do
     context 'when user inputs right' do
     x_coord = 0
     y_coord = 0
-    f = 'NORTH'
+    direction = 'NORTH'
       it 'should turn pacman but he should not move' do
         @pacman.right
         expect(@pacman.x_coord).to eq 0
         expect(@pacman.y_coord).to eq 0
-        expect(@pacman.f).to eq 'EAST'
+        expect(@pacman.direction).to eq 'EAST'
       end
     end
   end
-
   describe ".report" do
     context 'when user inputs report' do
       x_coord = 0
       y_coord = 1
-      f = 'NORTH'
+      direction = 'NORTH'
       it 'should return a string containing the location of pacman' do
-        @pacman.place(x_coord, y_coord, f = f)
+        @pacman.place(x_coord, y_coord, direction = direction)
         expect(@pacman.report).to be_a String
         expect(@pacman.x_coord).to eq 0
         expect(@pacman.y_coord).to eq 1
-        expect(@pacman.f).to eq 'NORTH'
+        expect(@pacman.direction).to eq 'NORTH'
       end
     end
   end
-
   describe ".move" do
     context 'when user inputs move' do
       x_coord = 2
       y_coord = 2
-      f = 'NORTH'
+      direction = 'NORTH'
       it 'should return a string containing the location of pacman' do
-        @pacman.place(x_coord, y_coord, f)
+        @pacman.place(x_coord, y_coord, direction)
         expect(@pacman.move).to eq 3
         expect(@pacman.x_coord).to eq 2
-        expect(@pacman.f).to eq 'NORTH'
+        expect(@pacman.direction).to eq 'NORTH'
       end
     end
   end
